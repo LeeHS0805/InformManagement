dee8101e7a017fed294b7138e763f6ed31dbb0fb
LeeHS0805 <504794559@qq.com>
2021-04-27 01:11:48 +0800

    delete ‘，’

--- src/app.config.js
+++ src/app.config.js
@@ -20,8 +20,7 @@ export default {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-    navigationBarTextStyle: 'black',
+    navigationBarTextStyle: 'black'  ,
   },
 
   tabBar: {
