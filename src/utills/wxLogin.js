bb44536df0501abca56a5130345b02c1c5a58e4b
liyouxia <62341055+liyouxia920@users.noreply.github.com>
2021-04-27 13:43:24 +0800

    1
    1

--- src/utills/wxLogin.js
+++ src/utills/wxLogin.js
@@ -4,6 +4,7 @@ import Taro from '@tarojs/taro'
 
 async function serverlogin(code){
   let val = await request('/login', "GET", {js_code: code});
+  if(val==null) return null;
   if (!verifyRequest(val.code, "code请求失败")) return null
   Taro.setStorageSync('session', val.data.session);
   Taro.setStorageSync('stuId', val.data.stuId);
@@ -11,35 +12,37 @@ async function serverlogin(code){
 }
 
 async function innerRequest() {
-  if (!Taro.getStorageSync('session') && Taro.getStorageSync('date')) {
+  //微信login请求
+  let val1 = await Taro.login()
 
-  }
-  try {
-    //微信login请求
-    let val1 = await Taro.login()
+  //向server传code
+  let val2 = await serverlogin(val1.code)
 
-    //向server传code
-    let val2 = await serverlogin(val1.code)
 
-    //判断是否注册
-    if (!val2.data.isRegister) {
-      val1 = await Taro.login()
-      //执行注册
-      await request("/bindUser", "post", {
-        realName: 'lxl',
-        stuId: 20195351,
-        jsCode:val1.code
-      })
-      val1 = await Taro.login()
-      await serverlogin(val1.code);
-    }
-  } catch (e) {
-    Taro.showToast({
-      title: '网络故障',
-      icon: 'error',
-      duration: 2500,
+
+  if(val2==null) return false;
+
+  
+
+  //判断是否注册
+  if (!val2.data.isRegister) {
+    // Taro.getUserProfile({
+    //   desc: '用于完善个人资料', 
+    //   success: (res) => {
+    //     Taro.showToast({
+    //       title: '成功授权'
+    //     })
+    //     console.log(res);
+    //     console.log("成功获取用户头像和个人信息");
+    //     console.log(res.userInfo.avatarUrl);
+    //     console.log(res.userInfo.nickName);
+    //   }
+    // });
+    let url = `../register/register`
+    Taro.navigateTo({
+        url,
     })
-  }
+  }else return true;
 }
 
 export default async function () {
